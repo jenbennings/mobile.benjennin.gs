670d73c4dc97c35ec3db45db67e6cd41e1ff78c0
Ben Jennings <ben@idealogue.io>
2014-10-17 18:59:47 +1100

    A bunch of new additions/improvements - Got jquery.wanker in there (#5) - Added tooltip on footer icons - Added scrollto - Added some new emoji assets

---
+++ assets/js/02-scrollto.js
@@ -0,0 +1,9 @@
+// $('#next').on('click', function(event) {
+//     var target = $('#slide-1');
+//     if( target.length ) {
+//         event.preventDefault();
+//         $('html, body').animate({
+//             scrollTop: target.offset().top
+//         }, 1000);
+//     }
+// });

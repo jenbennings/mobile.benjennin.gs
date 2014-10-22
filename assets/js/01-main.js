6c47e6635d78a8f287fa7029250dcddb2f47e45b
Matt Lenz <matt@mattlenz.com>
2014-10-22 15:49:51 +1100

    Clean up code a bit

--- assets/js/01-main.js
+++ assets/js/01-main.js
@@ -1,52 +1,29 @@
- /**
- * This demo was prepared for you by Petr Tichy - Ihatetomatoes.net
- * Want to see more similar demos and tutorials?
- * Help by spreading the word about Ihatetomatoes blog.
- * Facebook - https://www.facebook.com/ihatetomatoesblog
- * Twitter - https://twitter.com/ihatetomatoes
- * Google+ - https://plus.google.com/u/0/109859280204979591787/about
- * Article URL: http://ihatetomatoes.net/simple-parallax-scrolling-tutorial/
- */
+$(document).ready(function() {
+  $slides = $('.homeSlide');
+  $body   = $('body');
 
-( function( $ ) {
+  var adjustWindow = function() {
+    var windowHeight = $(window).height();
 
-  // Setup variables
-  $window = $(window);
-  $slide = $('.homeSlide');
-  $body = $('body');
+    // Keep minimum height 550
+    if(windowHeight <= 550) windowHeight = 550;
 
-    //FadeIn all sections
-  $body.imagesLoaded( function() {
-    setTimeout(function() {
-
-          // Resize sections
-          adjustWindow();
-
-          // Fade in sections
-        $body.removeClass('loading').addClass('loaded');
-
-    }, 800);
-  });
-
-  function adjustWindow(){
-
-    // Init Skrollr
-
-
-    // Get window size
-      winH = $window.height();
-
-      // Keep minimum height 550
-      if(winH <= 550) {
-      winH = 550;
-    }
-
-      // Resize our slides
-      $slide.height(winH);
-
-      // Refresh Skrollr after resizing our sections
-
-
+    // Resize our slides
+    return $slides.css({
+      height: windowHeight
+    });
   }
 
-} )( jQuery );
+  adjustWindow();
+
+  //FadeIn all sections
+  $body.imagesLoaded(function() {
+    setTimeout(function() {
+      // Resize sections
+      adjustWindow();
+
+      // Fade in sections
+      $body.removeClass('loading').addClass('loaded');
+    }, 800);
+  });
+});

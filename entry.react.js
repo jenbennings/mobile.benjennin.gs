1493cda69a812f6aa4427ae0af889b96146eaebd
Matt Lenz <matt@mattlenz.com>
2014-10-23 16:54:02 +1100

    Many changes

--- entry.react.js
+++ entry.react.js
@@ -4,45 +4,120 @@ var React = require('react');
 
 const SCREENS = require('./screens.js');
 
-var Screen = React.createClass({
+var ScreenProjectImages = React.createClass({
+  getInitialState: function() {
+    return {
+      currentImage: 0,
+    }
+  },
+  getInitialProps: function() {
+    return {
+      data: new Array
+    }
+  },
+  handleClick: function() {
+    var currentImage = this.state.currentImage;
+    currentImage++;
+
+    if(this.props.data.length < currentImage+1) currentImage = 0;
+
+    this.setState({ currentImage: currentImage });
+  },
+  render: function() {
+    var images = this.props.data.map(function(image, i) {
+      var style = {
+        backgroundImage: 'url("images/projects/'+ image +'")'
+      }
+
+      return (<div className="ScreenProjectImage" key={image} style={style}>
+        {/*<div className="ScreenProjectImageCounter">{i+1}</div>*/}
+      </div>);
+    }.bind(this));
+
+    var style = {
+      width: (this.props.data.length * 100) + 'vw',
+      left: (-100 * this.state.currentImage) + '%'
+    }
+
+    return (<div className="ScreenProjectImages" style={style} onClick={this.handleClick}>
+      {images}
+    </div>);
+  }
+});
+
+var ScreenProject = React.createClass({
   getInitialProps: function() {
     return {
       data: {
         title: null,
         description: null,
         link: null,
-        images: new Array,
+        images: new Array
+      }
+    }
+  },
+  getInitialState: function() {
+    return {
+      showInfo: false
+    }
+  },
+  handleClickShowInfo: function(event) {
+    event.preventDefault();
+    this.setState({ showInfo: !this.state.showInfo });
+  },
+  render: function() {
+    var hasProjectInfo = this.props.data.title || this.props.data.description || this.props.data.link;
+    return (<div className="ScreenProject" data-show-info={this.state.showInfo}>
+      {hasProjectInfo &&
+        <a className="ScreenProjectInfoToggle" href="#" onClick={this.handleClickShowInfo}>Info</a>
+      }
+      {hasProjectInfo &&
+        <div className="ScreenProjectInfo">
+          <div className="ScreenProjectInfoContent">
+            {this.props.data.title &&
+              <div className="ScreenProjectTitle"><h2>{this.props.data.title}</h2></div>
+            }
+            {this.props.data.description &&
+              <div className="ScreenProjectDescription"><p>{this.props.data.description}</p></div>
+            }
+            {this.props.data.link &&
+              <div className="ScreenProjectLink"><a target="_blank" href={this.props.data.link}>Check it</a></div>
+            }
+          </div>
+        </div>
+      }
+
+      {this.props.data.images.length &&
+        <ScreenProjectImages data={this.props.data.images} />
+      }
+    </div>);
+  }
+});
+
+var Screen = React.createClass({
+  getInitialProps: function() {
+    return {
+      data: {
+        project: false,
         html: null
       }
     }
   },
   render: function() {
-    var isProjectSlide = this.props.data.title || this.props.data.description;
-
     return (<div className="Screen">
       {this.props.data.html &&
         <div className="ScreenHTML" dangerouslySetInnerHTML={{__html: this.props.data.html}} />
       }
-      {isProjectSlide &&
-        <div className="ScreenProject">
-          {this.props.data.title &&
-            <div className="ScreenProjectTitle">{this.props.data.title}</div>
-          }
-          {this.props.data.description &&
-            <div className="ScreenProjectDescription">{this.props.data.description}</div>
-          }
-          {this.props.data.link &&
-            <div className="ScreenProjectLink"><a target="_blank" href={this.props.data.link}>Check it</a></div>
-          }
-        </div>
+      {this.props.data.project &&
+        <ScreenProject data={this.props.data.project} />
       }
     </div>);
   }
 });
 
 var Site = React.createClass({
-  renderScreen: function(screen, id) {
-    return (<Screen data={screen} id={id} />);
+  renderScreen: function(screen, i) {
+    return (<Screen key={i} data={screen} />);
   },
   render: function() {
     return (<div className="Site">
--- screens.js
+++ screens.js
@@ -1,10 +1,42 @@
 module.exports = [{
-  html: '<h1>I design <span class="mobile">mobile</span> interfaces.</h1>'
+  html: '<h1>I design <span data-icon="mobile"></span> interfaces.</h1>',
+  project: false
 }, {
-  title: 'Huxtaburger',
-  description: 'A responsive website that blah blah responsive yadda yadda. A collaboration between derp & derp.',
-  link: 'http://google.com/',
-  images: []
+  project: {
+    title: 'Huxtaburger',
+    description: 'A responsive website that blah blah responsive yadda yadda. A collaboration between derp & derp.',
+    link: 'http://google.com/',
+    images: ['huxtaburger-1.jpg', 'hive-1.jpg', '3.jpg']
+  }
 }, {
-  html: '<h1>Thank you and get amongst it.</h1>'
+  project: {
+    title: 'Hive',
+    description: 'A responsive website that blah blah responsive yadda yadda. A collaboration between derp & derp.',
+    link: 'http://google.com/',
+    images: ['hive-1.jpg']
+  }
+}, {
+  project: {
+    title: 'Lorem Ipsum',
+    description: 'A responsive website that blah blah responsive yadda yadda. A collaboration between derp & derp.',
+    link: 'http://google.com/',
+    images: ['3.jpg']
+  }
+}, {
+  project: {
+    title: 'Lorem Ipsum',
+    description: 'A responsive website that blah blah responsive yadda yadda. A collaboration between derp & derp.',
+    link: 'http://google.com/',
+    images: ['4.jpg']
+  }
+}, {
+  project: {
+    title: 'Lorem Ipsum',
+    description: 'A responsive website that blah blah responsive yadda yadda. A collaboration between derp & derp.',
+    link: 'http://google.com/',
+    images: ['5.jpg']
+  }
+}, {
+  html: '<h1>Thank you and get amongst it.</h1>',
+  project: false
 }];

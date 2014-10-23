e6ff79d2bb55f1e593118c7782c89ced3bfc2fdc
Matt Lenz <matt@mattlenz.com>
2014-10-23 14:54:59 +1100

    Add http server to gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ var livereload = require('gulp-livereload');
 
 const SCRIPTS_ENTRY = 'entry.react.js';
 const STYLES_ENTRY = 'entry.scss';
+const PUBLIC_DIR = 'public/';
+const SERVER_PORT = process.env.PORT || 8080;
 
 var browserify = require('gulp-browserify');
 var reactify = require('reactify');
@@ -14,7 +16,7 @@ gulp.task('scripts', function() {
     .pipe(plumber())
     .pipe(browserify({ transform: [reactify] }))
     .pipe(rename('bundle.js'))
-    .pipe(gulp.dest('public/'))
+    .pipe(gulp.dest(PUBLIC_DIR))
     .pipe(livereload());
 });
 
@@ -27,7 +29,7 @@ gulp.task('styles', function() {
     .pipe(sass())
     .pipe(autoprefixer())
     .pipe(rename('bundle.css'))
-    .pipe(gulp.dest('public/'))
+    .pipe(gulp.dest(PUBLIC_DIR))
     .pipe(livereload());
 });
 
@@ -36,4 +38,16 @@ gulp.task('watch', function() {
   gulp.watch(STYLES_ENTRY, ['styles']);
 });
 
-gulp.task('default', ['scripts', 'styles', 'watch']);
+var http = require('http');
+var ecstatic = require('ecstatic');
+var open = require('open');
+
+gulp.task('serve', function() {
+  var server = http.createServer(ecstatic({ root: PUBLIC_DIR }));
+  server.listen(SERVER_PORT, function() {
+    console.log('Server listening on port', SERVER_PORT);
+    open('http://localhost:'+SERVER_PORT);
+  });
+});
+
+gulp.task('default', ['scripts', 'styles', 'watch', 'serve']);

2f023044c8ce4e970e4aa5246a3bce79d0942c81
Ben Jennings <ben@idealogue.io>
2014-12-07 12:07:48 +1100

    Change wording

--- screens.js
+++ screens.js
@@ -24,7 +24,7 @@ module.exports = [{
     title: 'UOME',
     description: '<p>Experimental interface for<br>sending and receiving bitcoin.</p>',
     emoji: '<span data-icon="money"></span>',
-    outro: 'watch demo',
+    outro: 'demo video',
     link: 'http://google.com/',
     images: ['uome.jpg']
   }
